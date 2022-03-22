dbfd038cb71e26fec6f64264f6004e07dbb8dd21
gdhog79 <gdhog79@yahoo.com>
2022-03-22 17:51:05 -0400

    fix: :bug: Swap arrow function.

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,7 +9,8 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json';
 
-  getAlbum = (id: number) => this._http.get(this._albumUrl)
+  getAlbum (id: number) {
+    return this._http.get(this._albumUrl)
     .map(response => response.json());
-
+  }
 }

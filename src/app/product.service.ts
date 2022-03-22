fc259fb804d6c83dcc895aa2b98d17a596b48b8d
gdhog79 <gdhog79@yahoo.com>
2022-03-22 17:39:23 -0400

    feat: :sparkles:

---
+++ src/app/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) { }
+
+  _albumUrl = '../assets/album.json';
+
+  getAlbum = (id: number) => this._http.get(this._albumUrl)
+    .then(response => response.json());
+
+}

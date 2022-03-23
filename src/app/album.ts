a44f3767c4506147a2760e6cd77dc1095b128c04
gdhog79 <gdhog79@yahoo.com>
2022-03-23 17:57:15 -0400

    fix: :bug: Add semicolons.

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,8 +1,10 @@
 import { Track } from "./track";
 
 export interface Album {
-  name: string,
-  releaseDate: string,
-  coverImage: string,
-  tracks: Track[]
+  name: string;
+  releaseDate: string;
+  coverImage: string;
+  tracks: Track[];
 }
+
+export

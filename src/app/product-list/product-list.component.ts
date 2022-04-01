8650e9bffa501903e500b87d334c51646e168baa
gdhog79 <gdhog79@yahoo.com>
2022-04-01 18:03:34 -0400

    fix: :bug:

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
 import { ProductService } from '../product.service';
-
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
@@ -10,7 +10,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
 
-  constructor(private _productService) { }
+  constructor(private _productService: ProductService) { }
 
   ngOnInit() {
     this._productService.getProducts().subscribe(response => this.products = response);
